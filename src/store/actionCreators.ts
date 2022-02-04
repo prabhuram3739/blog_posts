45a6aade1cfa3773bdefe6e361c18d78f58572d5
prabhuram <prabhuram3739@gmail.com>
2022-02-04 12:16:20 +0530

    First commit

---
+++ src/store/actionCreators.ts
@@ -0,0 +1,44 @@
+import * as actionTypes from "./actionTypes";
+import axios from 'axios';
+
+export function addArticle(article: IArticle) {
+  const action: ArticleAction = {
+    type: actionTypes.ADD_ARTICLE,
+    article,
+  }
+
+  return simulateHttpRequest(action)
+}
+
+export function removeArticle(article: IArticle) {
+  const action: ArticleAction = {
+    type: actionTypes.REMOVE_ARTICLE,
+    article,
+  }
+  return simulateHttpRequest(action)
+}
+
+export function fetchArticle(article: IArticle) {
+    const action: ArticleAction = {
+      type: actionTypes.FETCH_ARTICLE,
+      article,
+    }
+    return simulateHttpRequest(action)
+  }
+
+export function simulateHttpRequest(action: ArticleAction) {
+  return (dispatch: DispatchType) => {
+    setTimeout(() => {
+      dispatch(action)
+    }, 500)
+  }
+}
+
+export function fetchArticleDetails() {
+    return function(dispatch: DispatchType) {
+      return axios.get("https://jsonplaceholder.typicode.com/posts")
+        .then(({ data }) => {
+        dispatch(setArticleDetails(data));
+      });
+    };
+  }
